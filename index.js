371842d9de8c65e99ebc7ea3dfde41884953fb34
Eric-Formiga <eric.13formiga@gmail.com>
2024-12-01 20:07:17 -0300

    feat: add create account initial setup

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-const inquirer = require('inquirer');
-const chalk = require('chalk');
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import fs from 'fs';
 
-const fs = require('fs');
+operation();
 
-console.log("sjkefh")
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: ['Criar conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair'],
+      },
+    ])
+    .then((answer) => {
+      const action = answer['action'];
+
+      if(action === 'Criar conta'){
+        createAccount()
+      }
+    })
+    .catch((error) => {
+      console.log(chalk.red('Ocorreu um erro: '), error);
+    });
+}
+
+function createAccount( ){
+  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco'))
+  console.log(chalk.green('Defina as opções da sua conta a seguir'))
+}
